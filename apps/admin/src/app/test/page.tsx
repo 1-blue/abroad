070dd0fa80dcfdcc98fbe80c07a9e85f61ea234f
상은 <1-blue98@naver.com>
2024-12-11 21:53:17 +0900

    test 13

--- apps/admin/src/app/test/page.tsx
+++ apps/admin/src/app/test/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@abroad/ui";
+
 const Page = () => {
-  return <div>test</div>;
+  return (
+    <article>
+      <div className="text-2xl font-bold text-red-500">test</div>
+
+      <Button>css 되나요</Button>
+    </article>
+  );
 };
 
 export default Page;
