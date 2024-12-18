a5e8eaffaa5fbe9f67b6e2aef4f04ef62e9c08a9
상은 <1-blue98@naver.com>
2024-12-18 20:13:04 +0900

    test 3

--- packages/apis/src/apis/axiosInstance.ts
+++ packages/apis/src/apis/axiosInstance.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL || "https://api.abroad0213.com",
   withCredentials: true,
 });
+
+console.log(
+  "🚀 log >> ",
+  process.env.NEXT_PUBLIC_SERVER_URL || "https://api.abroad0213.com"
+);
